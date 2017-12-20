7349c85f80ce133e9c311fbed0b0c0c4d9460276
zzj <397987442@qq.com>
2017-12-20 10:53:08 +0800

    增加自动热更新

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { UpdatePage } from '../update/update';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController){}
 
-  }
+  checkUpdate(event){
+    this.navCtrl.push(UpdatePage);
+    }
 
 }
